1e5c8ff75f492d8cf5bd7bcaa278793bfbf8dd5f
ismail <ismailmagdy88@gmail.com>
2023-07-20 08:03:57 +0300

    added not-found Routes Error Handling Middleware

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ app.use(express.static('./views'));
 app.use(express.json());
 app.use(logging('dev'));
 app.use('/api/v1/tasks',taskRouter);
-
+app.use((req,res,next)=>{
+    res.status(404).send("Route not found")
+})
 async function main(){
     try {
         await connectDB();
